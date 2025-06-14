8eb9e863db34e036c7fbd8883e9eb21fc233f6d2
cfal <cfal@users.noreply.github.com>
2025-06-14 16:13:21 +0900

    async_stream.rs: remove incorrect TcpStream shutdown impl from UnixStream

--- src/async_stream.rs
+++ src/async_stream.rs
@@ -35,24 +35,7 @@ impl AsyncStream for TcpStream {
 #[async_trait]
 impl AsyncStream for UnixStream {
     async fn try_shutdown(&mut self) -> std::io::Result<()> {
-        let _ = self.shutdown().await;
-
-        // Unfortunately, AsyncWriteExt::shutdown/AsyncWrite::poll_shutdown only ends up
-        // calling std::net::Shutdown::Write and seems to leave sockets in
-        // CLOSE-WAIT/TIME-WAIT/FIN-WAIT states.
-        // If the function signature for this returned self instead of &mut self,
-        // we could do:
-        //
-        // self.to_std().shutdown(std::net::Shutdown::Both)
-        //
-        // but tokio_native_tls has no function to allow going back to a
-        // tokio/mio/std stream.
-        // So do it the hacky way.
-
-        let std_stream = std::mem::ManuallyDrop::new(unsafe {
-            std::net::TcpStream::from_raw_fd(self.as_raw_fd())
-        });
-        std_stream.shutdown(std::net::Shutdown::Both)
+        self.shutdown().await
     }
 }
 
